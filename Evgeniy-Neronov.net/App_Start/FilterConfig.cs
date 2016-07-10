64d1f91d5c1c75c02e99f1a0a54efea1531ff652
Evgeniy <Evgeniy@PC-EVGENIY>
2016-07-10 15:13:40 +0300

    Initial Commit

---
+++ Evgeniy-Neronov.net/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Evgeniy_Neronov.net
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Evgeniy-Neronov.net/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Evgeniy_Neronov.net.MvcApplication" Language="C#" %>
